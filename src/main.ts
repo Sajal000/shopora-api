17c25d5ebf7de4dc6aa0700bc45e3cbf930bf7a2
Sajal Shrestha <shresthasajal03@gmail.com>
2025-03-02 17:29:57 -0500

    enable localhost

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
