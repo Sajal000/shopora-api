b40db6a918b35f89e9e508c498cb2ba11f440603
Sajal Shrestha <shresthasajal03@gmail.com>
2025-03-03 11:08:38 -0500

    main.ts config fixed

--- src/main.ts
+++ src/main.ts
@@ -8,38 +8,10 @@ async function bootstrap() {
   const allowedOrigins = ['http://localhost:3000'];
 
   app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('CORS not allowed'));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  });
-
-  app.use((req, res, next) => {
-    if (allowedOrigins.includes(req.headers.origin)) {
-      res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    }
-
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Authorization',
-    );
-
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-
-    next();
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   appCreate(app);
